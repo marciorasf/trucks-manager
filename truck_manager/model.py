366e8aac796d13de9b35a013fedc968cd990b137
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-06 19:01:57 -0300

    Add id to truck model.

--- truck_manager/model.py
+++ truck_manager/model.py
@@ -1,9 +1,11 @@
 from typing import Literal
+from uuid import uuid4
 
-from pydantic import BaseModel, Field, PositiveInt
+from pydantic import UUID4, BaseModel, Field, PositiveInt
 
 
 class Truck(BaseModel):
+    unique_id: UUID4 = uuid4()
     plate: str = Field(..., min_length=1, max_length=7)
     model_name: str = Field(..., min_length=1, max_length=20)
     tank_capacity: PositiveInt
