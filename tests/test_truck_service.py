deccc10713305fa4be7ab7a4e1b3ed7f302af488
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-06 20:46:11 -0300

    Parse TruckId using parse_truck_id.

--- tests/test_truck_service.py
+++ tests/test_truck_service.py
@@ -1,13 +1,22 @@
-from truck_manager.model import Truck
+from uuid import uuid4
+
+from truck_manager.model import Truck, parse_truck_id
 from truck_manager.truck_repository import TruckRepositoryInMemory
 from truck_manager.truck_service import add_truck
 
 
 def test_add_truck() -> None:
-    identifier = add_truck(
-        Truck(plate="AAA1111", model_name="civic", tank_capacity=100, status="OK"),
+    expected_id = parse_truck_id(uuid4())
+
+    result = add_truck(
+        Truck(
+            identifier=expected_id,
+            plate="AAA1111",
+            model_name="civic",
+            tank_capacity=100,
+            status="OK",
+        ),
         TruckRepositoryInMemory(),
     )
 
-    print(identifier)
-    assert True
+    assert result == expected_id
--- truck_manager/model.py
+++ truck_manager/model.py
@@ -6,8 +6,13 @@ from pydantic import UUID4, BaseModel, Field, PositiveInt
 TruckId = NewType("TruckId", UUID4)
 
 
+def parse_truck_id(value: UUID4) -> TruckId:
+    """All TruckIds must be created using this function."""
+    return TruckId(value)
+
+
 class Truck(BaseModel):
-    identifier: TruckId = TruckId(uuid4())
+    identifier: TruckId = parse_truck_id(uuid4())
     plate: str = Field(..., min_length=1, max_length=7)
     model_name: str = Field(..., min_length=1, max_length=20)
     tank_capacity: PositiveInt
